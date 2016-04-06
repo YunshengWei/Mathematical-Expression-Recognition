96c7282b7511ed2125404744d03763ac9246fac6
hwang381 <hwang381@gatech.edu>
2016-04-06 16:00:12 -0500

    draw on canvas

--- src/static/index.js
+++ src/static/index.js
@@ -1,1 +1,125 @@
-console.log("test");
+// Setup canvas
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+context.strokeStyle = '#222222';
+context.lineWidth = 2;
+
+// Global state
+var drawing;
+var pos;
+var lastPos;
+var strokes;
+var currentStroke;
+function clearCanvas() {
+    canvas.width = canvas.width;
+    drawing = false;
+    pos = {x: 0, y: 0};
+    lastPos = pos;
+    strokes = [];
+    currentStroke = [];
+}
+clearCanvas();
+
+// Mouse events
+function getMousePos(canvas, e) {
+    var rectangle = canvas.getBoundingClientRect();
+    return {
+        x: e.clientX - rectangle.left,
+        y: e.clientY - rectangle.top
+    }
+}
+canvas.addEventListener('mousedown', function (e) {
+    drawing = true;
+    lastPos = getMousePos(canvas, e);
+});
+canvas.addEventListener('mouseup', function (e) {
+    drawing = false;
+
+});
+canvas.addEventListener('mousemove', function (e) {
+    pos = getMousePos(canvas, e);
+});
+
+// Touch events
+function getTouchPos(canvas, e) {
+    var rectangle = canvas.getBoundingClientRect();
+    return {
+        x: e['touches'][0].clientX - rectangle.left,
+        y: e['touches'][0].clientY - rectangle.top
+    }
+}
+canvas.addEventListener('touchstart', function (e) {
+    var touch = e['touches'][0];
+    var mouseEvent = new MouseEvent("mousedown", {
+        clientX: touch.clientX,
+        clientY: touch.clientY
+    });
+    canvas.dispatchEvent(mouseEvent);
+});
+canvas.addEventListener("touchend", function (e) {
+    canvas.dispatchEvent(new MouseEvent("mouseup", {}));
+});
+canvas.addEventListener("touchmove", function (e) {
+    var touch = e['touches'][0];
+    var mouseEvent = new MouseEvent("mousemove", {
+        clientX: touch.clientX,
+        clientY: touch.clientY
+    });
+    canvas.dispatchEvent(mouseEvent);
+});
+
+// Prevent scrolling when touching the canvas
+document.body.addEventListener("touchstart", function (e) {
+    if (e.target === canvas) {
+        e.preventDefault();
+    }
+});
+document.body.addEventListener("touchend", function (e) {
+    if (e.target === canvas) {
+        e.preventDefault();
+    }
+});
+document.body.addEventListener("touchmove", function (e) {
+    if (e.target === canvas) {
+        e.preventDefault();
+    }
+});
+
+// Get animation and draw on canvas
+window.requestAnimFrame = (function (callback) {
+    return window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.oRequestAnimationFrame ||
+        window.msRequestAnimaitonFrame ||
+        function (callback) {
+            window.setTimeout(callback, 1000 / 60);
+        };
+})();
+function renderPosOnCanvas() {
+    if (drawing) {
+        context.moveTo(lastPos.x, lastPos.y);
+        context.lineTo(pos.x, pos.y);
+        context.stroke();
+    }
+}
+function savePosToStrokes() {
+    if (drawing) {
+        currentStroke.push(pos);
+    } else if (currentStroke.length !== 0) {
+        strokes.push(currentStroke);
+        currentStroke = [];
+    }
+}
+(function renderLoop() {
+    requestAnimationFrame(renderLoop);
+    renderPosOnCanvas();
+    savePosToStrokes();
+    lastPos = pos;
+})();
+
+// Submit button
+$('#submit').click(function () {
+    console.log(strokes);
+    clearCanvas();;
+});
