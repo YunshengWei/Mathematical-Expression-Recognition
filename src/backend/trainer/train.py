ffc6dbbaff846f5e260573c23e80fa7ed93673c2
hwang381 <hwang381@gatech.edu>
2016-05-01 14:58:57 -0500

    more changes

--- src/backend/trainer/train.py
+++ src/backend/trainer/train.py
@@ -23,7 +23,7 @@ import time
 
 import numpy as np
 import tensorflow as tf
-from model import *
+from .model import inference, loss, train
 
 FLAGS = tf.app.flags.FLAGS
 
@@ -36,78 +36,73 @@ tf.app.flags.DEFINE_integer('max_steps', 1000000,
 tf.app.flags.DEFINE_boolean('log_device_placement', False,
                             """Whether to log device placement.""")
 
+if tf.gfile.Exists(FLAGS.train_dir):
+    tf.gfile.DeleteRecursively(FLAGS.train_dir)
+tf.gfile.MakeDirs(FLAGS.train_dir)
 
-def train():
-    """Train for a number of steps."""
-    with tf.Graph().as_default():
-        global_step = tf.Variable(0, trainable=False)
+"""Train for a number of steps."""
+with tf.Graph().as_default():
+    global_step = tf.Variable(0, trainable=False)
 
-        # Get images and labels
-        # images, labels = model.distorted_inputs()
-        # todo
+    # Get images and labels
+    # images, labels = model.distorted_inputs()
+    # todo
 
-        # Build a Graph that computes the logits predictions from the
-        # inference model.
-        logits = inference(images)
+    # Build a Graph that computes the logits predictions from the
+    # inference model.
+    logits = inference(images)
 
-        # Calculate loss.
-        loss = loss(logits, labels)
+    # Calculate loss.
+    loss = loss(logits, labels)
 
-        # Build a Graph that trains the model with one batch of examples and
-        # updates the model parameters.
-        train_op = train(loss, global_step)
+    # Build a Graph that trains the model with one batch of examples and
+    # updates the model parameters.
+    train_op = train(loss, global_step)
 
-        # Create a saver.
-        saver = tf.train.Saver(tf.all_variables())
+    # Create a saver.
+    saver = tf.train.Saver(tf.all_variables())
 
-        # Build the summary operation based on the TF collection of Summaries.
-        summary_op = tf.merge_all_summaries()
+    # Build the summary operation based on the TF collection of Summaries.
+    summary_op = tf.merge_all_summaries()
 
-        # Build an initialization operation to run below.
-        init = tf.initialize_all_variables()
+    # Build an initialization operation to run below.
+    init = tf.initialize_all_variables()
 
-        # Start running operations on the Graph.
-        sess = tf.Session(config=tf.ConfigProto(
-            log_device_placement=FLAGS.log_device_placement))
-        sess.run(init)
+    # Start running operations on the Graph.
+    sess = tf.Session(config=tf.ConfigProto(
+        log_device_placement=FLAGS.log_device_placement))
+    sess.run(init)
 
-        # Start the queue runners.
-        tf.train.start_queue_runners(sess=sess)
+    # Start the queue runners.
+    tf.train.start_queue_runners(sess=sess)
 
-        summary_writer = tf.train.SummaryWriter(FLAGS.train_dir, sess.graph)
+    summary_writer = tf.train.SummaryWriter(FLAGS.train_dir, sess.graph)
 
-        for step in xrange(FLAGS.max_steps):
-            start_time = time.time()
-            _, loss_value = sess.run([train_op, loss])
-            duration = time.time() - start_time
+    for step in xrange(FLAGS.max_steps):
+        start_time = time.time()
+        _, loss_value = sess.run([train_op, loss])
+        duration = time.time() - start_time
 
-            assert not np.isnan(loss_value), 'Model diverged with loss = NaN'
+        assert not np.isnan(loss_value), 'Model diverged with loss = NaN'
 
-            if step % 10 == 0:
-                num_examples_per_step = FLAGS.batch_size
-                examples_per_sec = num_examples_per_step / duration
-                sec_per_batch = float(duration)
+        if step % 10 == 0:
+            num_examples_per_step = FLAGS.batch_size
+            examples_per_sec = num_examples_per_step / duration
+            sec_per_batch = float(duration)
 
-                format_str = ('%s: step %d, loss = %.2f (%.1f examples/sec; %.3f '
-                              'sec/batch)')
-                print(format_str % (datetime.now(), step, loss_value,
-                                    examples_per_sec, sec_per_batch))
+            format_str = ('%s: step %d, loss = %.2f (%.1f examples/sec; %.3f '
+                          'sec/batch)')
+            print(format_str % (datetime.now(), step, loss_value,
+                                examples_per_sec, sec_per_batch))
 
-            if step % 100 == 0:
-                summary_str = sess.run(summary_op)
-                summary_writer.add_summary(summary_str, step)
+        if step % 100 == 0:
+            summary_str = sess.run(summary_op)
+            summary_writer.add_summary(summary_str, step)
 
-            # Save the model checkpoint periodically.
-            if step % 1000 == 0 or (step + 1) == FLAGS.max_steps:
-                checkpoint_path = os.path.join(FLAGS.train_dir, 'model.ckpt')
-                saver.save(sess, checkpoint_path, global_step=step)
-
-
-def main(argv=None):  # pylint: disable=unused-argument
-    if tf.gfile.Exists(FLAGS.train_dir):
-        tf.gfile.DeleteRecursively(FLAGS.train_dir)
-    tf.gfile.MakeDirs(FLAGS.train_dir)
-    train()
+        # Save the model checkpoint periodically.
+        if step % 1000 == 0 or (step + 1) == FLAGS.max_steps:
+            checkpoint_path = os.path.join(FLAGS.train_dir, 'model.ckpt')
+            saver.save(sess, checkpoint_path, global_step=step)
 
 
 if __name__ == '__main__':
